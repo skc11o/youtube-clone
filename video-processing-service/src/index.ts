5e7a5e0c8a78c7b0d4e556b3451ab9df7904d09d
Saurav <skaii@Sauravs-MacBook-Pro.local>
2025-01-31 13:00:03 +0000

    local host tested and processed vide

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,13 +1,30 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg"; 
 
 const app = express();
-const port = 3000;
+app.use(express.json());
+app.post("/process-video", (req, res) => {
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send('Bad Request: Missing file path');
+      }
 
+      ffmpeg(inputFilePath)
+      .outputOptions('-vf', 'scale=-1:360') // 360p
+      .on('end', function() {
+          console.log('Processing finished successfully');
+          res.status(200).send('Processing finished successfully');
+      })
+      .on('error', function(err: any) {
+          console.log('An error occurred: ' + err.message);
+          res.status(500).send('An error occurred: ' + err.message);
+      })
+      .save(outputFilePath);
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Video processing service running on http://localhost:${port}`);
 });
